e5511bf6a872ae891a7e9bb0badaf978ad4872ae
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-30 01:24:28 +0900

    feat post

--- src/main/java/web/global/config/SecurityConfig.java
+++ src/main/java/web/global/config/SecurityConfig.java
@@ -11,12 +11,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/signup", "/users/login").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
--- src/main/java/web/user/dto/UserLoginDto.java
+++ src/main/java/web/user/dto/UserLoginDto.java
@@ -1,6 +1,7 @@
 package web.user.dto;
 
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Getter @Setter
 public class UserLoginDto {
